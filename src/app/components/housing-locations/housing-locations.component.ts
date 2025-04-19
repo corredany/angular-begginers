07ca84609248ed1bf5aa23147c4eab205151aaa1
Arturo <correosdany@outlook.com>
2025-04-18 23:23:34 -0600

    Parte 3

--- src/app/components/housing-locations/housing-locations.component.ts
+++ src/app/components/housing-locations/housing-locations.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostAttributeToken, Input } from '@angular/core';
+import { HousingLocations } from '../housing-locations';
 
 @Component({
   selector: 'app-housing-locations',
@@ -7,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './housing-locations.component.css'
 })
 export class HousingLocationsComponent {
+
+  @Input() housingLocations!: HousingLocations;
 
 }
